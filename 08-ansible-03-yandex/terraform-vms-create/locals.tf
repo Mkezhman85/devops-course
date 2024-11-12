1d2f6b4b70c41b555ba8fda4f5243306a3530659
Sergei Yurkin <mkezhman85@gmail.com>
2024-11-12 14:03:37 +0300

    08-ansible-yandex homework

---
+++ 08-ansible-03-yandex/terraform-vms-create/locals.tf
@@ -0,0 +1,5 @@
+locals {
+  netology-clickhouse = var.vm_clickhouse_yandex_compute_instance_name
+  netology-vector = var.vm_vector_yandex_compute_instance_name
+  netology-lighthouse = var.vm_lighthouse_yandex_compute_instance_name
+}
